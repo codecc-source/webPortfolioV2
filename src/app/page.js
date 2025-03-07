8d080e4a41ec70e6061e2227c88a0d52d14b0f55
codecc-source <carlitotingson@gmail.com>
2025-03-08 02:32:24 +0800

    Added magic UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Navbar from "./components/navBar";
+import { InteractiveGridPattern } from "@/components/magicui/interactive-grid-pattern";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-cover flex flex-col items-center justify-center px-6 bg-[url('/dark.svg')] pt-20 md:pt-20">
+      
       <Navbar/>
       <div className="border-2 border-white px-6 py-3 rounded-lg">
         <h1 className="text-[3rem] font-custom font-bold text-center text-white">
@@ -18,6 +20,7 @@ export default function Home() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec quam libero. Proin id mauris vitae nisi dignissim blandit. Aliquam feugiat, est dapibus maximus venenatis, justo neque commodo odio, eget tincidunt dui nulla in odio. Maecenas sollicitudin velit ac varius sodales. Aliquam sodales aliquam purus in placerat. Ut malesuada elit eget odio varius rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris pharetra arcu neque, eget semper neque finibus id. Nam posuere nisl viverra aliquam tristique.        
         </p>
       </div>
+      <InteractiveGridPattern/> {/* Fix this lmao */}
 
     </div>
   );
